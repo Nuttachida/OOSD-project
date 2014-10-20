64c731887e700df1af66e48e28781b7301582fc2
Aom W. <peck_sudza@hotmail.com>
2014-10-20 11:45:22 +0700

    SpreadFire3
    edit form spreadfire2

---
+++ SpreadFire3/src/spreadfire/Graphic.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spreadfire;
+
+
+import java.awt.*;
+    import javax.swing.*;
+
+/**
+ *
+ * @author aom
+ */
+public class Graphic extends JFrame{
+
+        JButton start = new JButton("start");
+        JButton stop = new JButton("stop");
+        JButton reset = new JButton("reset");
+        
+        public Graphic(){
+            JPanel panel2 = new JPanel();
+            //panel.setLayout(new GridLayout(2,2));
+            panel2.add(start);
+            panel2.add(stop);
+            panel2.add(reset);
+            this.add(panel2,BorderLayout.EAST);
+           // spread.addActionListener(new spreadListener()); พอกดปุ่มแล้วรัน
+            
+        }
+
+        public static void main(String[] args) throws Exception { 
+        
+            
+            JFrame frame = new Graphic(); // big frame
+            frame.setSize(600,600);
+            frame.setLocation(100, 100);
+            frame.setTitle("Spreading of Fire");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            //ใส่สี
+            ColorPanel panel = new ColorPanel(Color.BLACK);
+            Container pane = frame.getContentPane();
+            pane.add(panel);
+            
+            
+        }   
+        
+        
+        
+    }
+
+            
+     
+
+
+
+
---
+++ SpreadFire3/src/spreadfire/SpreadFire.java
@@ -0,0 +1,107 @@
+
+package spreadfire;
+
+import java.util.*;
+import javax.swing.JPanel;
+
+public class SpreadFire {
+    static int two = 1;
+    
+    static ArrayList<Integer> keepi = new ArrayList<>();
+    static ArrayList<Integer> keepj = new ArrayList<>();
+       
+    static double prob = 1; //กำหนด prob
+    static double[][] randomProb =new double[20][20]; //0-1
+    static int[][] size = new int[20][20]; //size keep 0,1,2
+    
+    public static void main(String[] args) {
+        
+        // DO TREE = 1 
+        for(int i =0;i<size.length;i++){
+            for(int j=0 ; j<size.length;j++){
+               if(j==0 || i==0 || j==size.length-1 || i==size.length-1){//border
+                    size[i][j]= 0;
+                }else{size[i][j] =1;}
+            }
+        }
+       
+       //randomProb;
+       for(int i=0 ;i<randomProb.length;i++){
+           for(int j=0;j<randomProb.length;j++){
+               randomProb[i][j] = Math.random();
+           }
+       }
+       while(two > 0){
+           prepare(size);
+           print(size);
+       }
+     
+     
+     JPanel show = new CreateGui(size);
+     
+           
+    }
+    public static void print(int [][] c){
+        for (int[] c1 : c) {
+            for (int j = 0; j<c.length; j++) {
+                System.out.print(c1[j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------------------------");
+    }
+    
+    public static void prepare(int[][] b){
+     keepi.clear();
+     keepj.clear();
+       
+        b[13][12]=2; // DO BURN POINT
+       
+        for(int i=1;i<b.length-1;i++){ //KEEP BURN POINT
+            for(int j=1;j<b.length-1;j++){
+                if(b[i][j]==2){
+                keepi.add(i);
+                keepj.add(j);
+                }                
+            }
+        }
+        burn(keepi,keepj,b); 
+    }
+    
+    public static void burn(ArrayList i, ArrayList j,int[][] b){
+        two=0;
+        for(int y =0;y<i.size();y++){
+            if(randomProb[(int)i.get(y)-1][(int)j.get(y)] < prob){ //NORTH
+               if( b[(int)i.get(y)-1][(int)j.get(y)]==1){
+                   b[(int)i.get(y)-1][(int)j.get(y)]=2;
+                   two++;
+               }
+               //b[(int)i.get(y)-1][(int)j.get(y)]= b[(int)i.get(y)-1][(int)j.get(y)]==1 ? 2 : b[(int)i.get(y)-1][(int)j.get(y)];
+            }
+            
+            if(randomProb[(int)i.get(y)+1][(int)j.get(y)] < prob){ //SOUTH
+                
+                if(b[(int)i.get(y)+1][(int)j.get(y)]==1){
+                    b[(int)i.get(y)+1][(int)j.get(y)]=2;
+                    two++;
+                }
+               // b[(int)i.get(y)+1][(int)j.get(y)]= b[(int)i.get(y)+1][(int)j.get(y)]==1 ? 2 : b[(int)i.get(y)+1][(int)j.get(y)];
+            }
+            if(randomProb[(int)i.get(y)][(int)j.get(y)-1] < prob){ //WEST
+                if(b[(int)i.get(y)][(int)j.get(y)-1]==1 ) {
+                    b[(int)i.get(y)][(int)j.get(y)-1]=2; 
+                    two++;
+                }
+                // b[(int)i.get(y)][(int)j.get(y)-1]= b[(int)i.get(y)][(int)j.get(y)-1]==1 ? 2 : b[(int)i.get(y)][(int)j.get(y)-1];
+            }
+            if(randomProb[(int)i.get(y)][(int)j.get(y)+1 ]< prob){ //EAST
+                if(b[(int)i.get(y)][(int)j.get(y)+1]==1){
+                    b[(int)i.get(y)][(int)j.get(y)+1]=2;
+                    two++;
+                }
+                //b[(int)i.get(y)][(int)j.get(y)+1]= b[(int)i.get(y)][(int)j.get(y)+1]==1 ? 2 : b[(int)i.get(y)][(int)j.get(y)+1];
+            }
+            b[(int)i.get(y)][(int)j.get(y)] = 0;
+        }
+    }
+}
