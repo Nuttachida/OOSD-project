0dd5061aa44fe2ded28d20a918a60ce846524e10
Aom W. <peck_sudza@hotmail.com>
2014-11-21 23:59:12 +0700

    SpreadFire_Refactor_FINISH

---
+++ SpreadFire_Refactor_FINISH/src/spreadfire/SpreadFire.java
@@ -0,0 +1,13 @@
+package spreadfire;
+
+public class SpreadFire {
+    public static void main(String[] args) {
+        
+        CreateGUI show = new CreateGUI();
+        show.setVisible(true);
+        
+        SpreadFire app = new SpreadFire();
+        
+        
+    }
+}
